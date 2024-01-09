701ab0fcab0384296c5242635b66c461abb021fb
JS Ying <jhihsiying@gmail.com>
2024-01-10 01:49:33 +0800

    update trainType dark theme

--- components/train-time-table/TR/TrTrainType.tsx
+++ components/train-time-table/TR/TrTrainType.tsx
@@ -3,42 +3,66 @@ const TrTrainType = ({ code, trainTypeName }) => {
     <>
       {/* 自強 */}
       {["3", "11"].includes(code) && (
-        <span className={"rounded bg-teal-500 px-1 py-0.5 text-white"}>
+        <span
+          className={
+            "rounded bg-teal-500 px-1 py-0.5 text-white dark:bg-teal-500/80"
+          }
+        >
           {trainTypeName}
         </span>
       )}
 
       {/* 區間 */}
       {["6", "7", "10"].includes(code) && (
-        <span className={"rounded bg-sky-500 px-1 py-0.5 text-white"}>
+        <span
+          className={
+            "rounded bg-sky-500 px-1 py-0.5 text-white dark:bg-sky-500/80"
+          }
+        >
           {trainTypeName}
         </span>
       )}
 
       {/* 大魯閣 */}
       {["1"].includes(code) && (
-        <span className={"rounded bg-indigo-500 px-1 py-0.5 text-white"}>
+        <span
+          className={
+            "rounded bg-indigo-500 px-1 py-0.5 text-white dark:bg-indigo-500/80"
+          }
+        >
           {trainTypeName}
         </span>
       )}
 
       {/* 普悠瑪 */}
       {["2"].includes(code) && (
-        <span className={"rounded bg-rose-500 px-1 py-0.5 text-white"}>
+        <span
+          className={
+            "rounded bg-rose-500 px-1 py-0.5 text-white dark:bg-rose-500/80"
+          }
+        >
           {trainTypeName}
         </span>
       )}
 
       {/* 莒光號 */}
       {["4"].includes(code) && (
-        <span className={"rounded bg-amber-500 px-1 py-0.5 text-white"}>
+        <span
+          className={
+            "rounded bg-amber-500 px-1 py-0.5 text-white dark:bg-amber-500/80"
+          }
+        >
           {trainTypeName}
         </span>
       )}
 
       {/* 復興 */}
       {["5"].includes(code) && (
-        <span className={"rounded bg-amber-500 px-1 py-0.5 text-white"}>
+        <span
+          className={
+            "rounded bg-amber-500 px-1 py-0.5 text-white dark:bg-amber-500/80"
+          }
+        >
           {trainTypeName}
         </span>
       )}
