11072b338d697e8bd0ac23875e4e9a178f7e17df
JS Ying <jhihsiying@gmail.com>
2024-02-24 19:17:08 +0800

    add DelayDot

--- components/train-time-table/TR/TrDelay.tsx
+++ components/train-time-table/TR/TrDelay.tsx
@@ -2,6 +2,21 @@ import { useTranslation } from "next-i18next";
 import { FC } from "react";
 import { TrDelayInfo } from "../../../types/tr-train-time-table";
 
+interface DelayDotProps {
+  isGreen: boolean;
+}
+
+const DelayDot: FC<DelayDotProps> = ({ isGreen }) => {
+  return (
+    <>
+      {isGreen && (
+        <span className="dot bg-emerald-600 dark:bg-emerald-400"></span>
+      )}
+      {!isGreen && <span className="dot bg-red-600 dark:bg-red-400"></span>}
+    </>
+  );
+};
+
 interface TrDelayProps {
   dataList: TrDelayInfo[];
 }
@@ -14,13 +29,15 @@ const TrDelay: FC<TrDelayProps> = ({ dataList }) => {
       {dataList?.length > 0 && (
         <>
           {dataList[0].DelayTime === 0 ? (
-            <div className="text-sm text-emerald-600 dark:text-emerald-400">
+            <span className="relative text-sm text-emerald-600 dark:text-emerald-400">
               {t("onTime")}
-            </div>
+              <DelayDot isGreen={true}></DelayDot>
+            </span>
           ) : (
-            <div className="text-sm text-red-600 dark:text-red-400">
+            <span className="relative text-sm text-red-600 dark:text-red-400">
               {t("delay")} {dataList[0].DelayTime} {t("minute")}
-            </div>
+              <DelayDot isGreen={false}></DelayDot>
+            </span>
           )}
         </>
       )}
