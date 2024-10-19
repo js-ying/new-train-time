a8a635bc93fa47897ac98875e0df4567ec591c3e
JS Ying <jhihsiying@gmail.com>
2024-10-19 18:31:13 +0800

    add AdBanner: BottomBanner

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -1,7 +1,53 @@
 import { useTranslation } from "next-i18next";
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 
-const AdBanner: FC = () => {
+const BottomBanner: FC = () => {
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+
+    const timer = setTimeout(() => {
+      setOpen(true);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div
+      className="fixed bottom-0 left-0 right-0 z-10 mx-auto max-w-[728px] p-4 transition-all duration-300 ease-out"
+      style={{ opacity: open ? 1 : 0 }}
+    >
+      <div
+        className="relative h-[108px] rounded-md border border-solid border-zinc-700 p-2
+  transition duration-150 ease-out dark:border-zinc-200"
+      >
+        <button
+          onClick={handleClose}
+          className="absolute -right-3 -top-3 flex h-6 w-6 items-center justify-center rounded-full
+            bg-zinc-700 text-zinc-200 shadow-md hover:text-zinc-400 dark:bg-white
+            dark:text-zinc-700 dark:hover:text-zinc-500"
+        >
+          ✕
+        </button>
+        <ins
+          className="adsbygoogle block h-full w-full"
+          data-ad-client="ca-pub-7992139989807299"
+          data-ad-slot="1622239321"
+        ></ins>
+      </div>
+    </div>
+  );
+};
+
+const TrainInfoBanner: FC = () => {
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -12,7 +58,7 @@ const AdBanner: FC = () => {
     <>
       <div
         className="relative h-[268px] rounded-md border border-solid border-zinc-700 p-2
-        transition duration-150 ease-out dark:border-zinc-200 md:h-[108px]"
+  transition duration-150 ease-out dark:border-zinc-200 md:h-[108px]"
       >
         <ins
           className="adsbygoogle block h-full w-full"
@@ -23,6 +69,15 @@ const AdBanner: FC = () => {
       <div className="mt-1 whitespace-pre-line text-xs text-zinc-500 dark:text-zinc-400">
         {t("adMsg")}
       </div>
+    </>
+  );
+};
+
+const AdBanner: FC = () => {
+  return (
+    <>
+      <BottomBanner />
+      <TrainInfoBanner />
     </>
   );
 };
