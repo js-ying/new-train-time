138dcdad49ae14581e638a1d6e734ba1227ff0d9
JS Ying <jhihsiying@gmail.com>
2024-10-25 00:49:49 +0800

    add TdxAlert

--- components/train-time-table/NoTrainData.tsx
+++ components/train-time-table/NoTrainData.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "next-i18next";
 import { FC } from "react";
 import { LocaleEnum } from "../../enums/LocaleEnum";
 import DateUtils from "../../utils/DateUtils";
+import TdxAlert from "../TdxAlert";
 
 interface NoTrainDataProps {
   isApiHealth: boolean;
@@ -25,14 +26,19 @@ const NoTrainData: FC<NoTrainDataProps> = ({ isApiHealth, alertMsg }) => {
       )}
 
       {!isApiHealth && (
-        <Alert severity="error" variant="outlined">
-          <div className="font-bold">
-            {t("noTrainDataDueToApiErrorMsg")}
-            {`${i18n.language === LocaleEnum.TW ? "" : " "}`}
-            {`(${DateUtils.getCurrentDatetime()})`}
+        <>
+          <div className="mb-4">
+            <TdxAlert />
           </div>
-          <div>{alertMsg.toString()}</div>
-        </Alert>
+          <Alert severity="error" variant="outlined">
+            <div className="font-bold">
+              {t("noTrainDataDueToApiErrorMsg")}
+              {`${i18n.language === LocaleEnum.TW ? "" : " "}`}
+              {`(${DateUtils.getCurrentDatetime()})`}
+            </div>
+            <div>{alertMsg.toString()}</div>
+          </Alert>
+        </>
       )}
     </div>
   );
