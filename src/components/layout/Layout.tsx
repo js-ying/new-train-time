18ca0054a1d25f001a61815605b45e06357f61d8
jsying <jhihsiying@gmail.com>
2026-04-16 13:01:10 +0800

    add privacy and terms sr-only

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,8 @@
 import LocaleChange from "@/components/common/LocaleChange";
 import ThemeSwitch from "@/components/common/ThemeSwitch";
 import WebTitle from "@/components/common/WebTitle";
+import { useTranslation } from "next-i18next";
+import Link from "next/link";
 import { FC } from "react";
 import Sidebar from "../sidebar/Sidebar";
 
@@ -10,6 +12,8 @@ interface LayoutProps {
 }
 
 const Layout: FC<LayoutProps> = ({ children, title = "" }) => {
+  const { t } = useTranslation();
+
   return (
     <div className="mx-auto max-w-6xl px-4 py-6">
       <div className="relative mb-6">
@@ -29,6 +33,15 @@ const Layout: FC<LayoutProps> = ({ children, title = "" }) => {
         </div>
       </div>
       {children}
+      {/* 無障礙 / SEO 用 footer */}
+      <footer className="sr-only">
+        <nav>
+          <Link href="/features">{t("featureIntroductionMenu")}</Link>
+          <Link href="/updates">{t("updateAnnouncementsMenu")}</Link>
+          <Link href="/terms">{t("termsOfService")}</Link>
+          <Link href="/privacy">{t("privacyPolicy")}</Link>
+        </nav>
+      </footer>
     </div>
   );
 };
